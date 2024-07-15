c288469ea0c520c05f8369cc11b88beaf4f3de10
Martha Ligia De Luque Aaron <martha.deluque@lever.co>
2024-07-15 11:03:18 -0500

    WKL-ADT Algorithms Study - container with most water

---
+++ wkl-adt/container-with-most-water.ts
@@ -0,0 +1,20 @@
+function maxArea(height: number[]): number {
+    let low = 0;
+    let high = height.length - 1;
+    let ans = 0;
+  
+    while (low <= high) {
+      const length = Math.min(height[low], height[high]);
+      const breadth = high - low;
+      const area = length * breadth;
+      if (ans < area){
+          ans = area;
+      }
+      if (height[low] < height[high]){
+          low++;
+      } else {
+          high--;
+      }
+    }
+    return ans;
+  };
